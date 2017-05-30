8e25764eba859b8d80db73c5e74c0361c5a3df3d
Your Full Name <youremailaddress@example.com>
2017-05-30 11:29:10 -0400

    add route for github username

--- src/Github.js
+++ src/Github.js
@@ -34,6 +34,7 @@ class Github extends Component {
                 </form>
 
                 <Route exact path='/github' render={() => <h3>Please enter a username to search on github</h3>}/>
+                <Route path='/github/:username' render={({ match }) => <h3>Great job you search for {match.params.username}</h3>} /> 
             </div>
         )
     }
