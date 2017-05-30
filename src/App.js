237ba8c4b372cf8fa2084531e180bfa79ce202e3
Your Full Name <youremailaddress@example.com>
2017-05-30 13:44:10 -0400

    Added nasa API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import Github from './Github'
 import Instagram from './Instagram'
+import Nasa from './Nasa'
 import './App.css';
 
 class App extends Component {
@@ -17,6 +18,11 @@ class App extends Component {
           <ul className='nav-links'>
             <li>
               <NavLink to='/github'>GitHub API</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/nasa'}>Nasa API</NavLink>
+            </li>
+            <li>
               <NavLink to='/instagram'>Instgram API</NavLink>
             </li>
           </ul>
@@ -24,6 +30,7 @@ class App extends Component {
         <Switch>
           <Route path="/github" component={Github}/>
           <Route path='/instagram' component={Instagram}/>
+          <Route path='/nasa' component={Nasa} />
           <Route render={() => <p>To get started, click one of the links above </p>}/>
         </Switch>
       </div>
