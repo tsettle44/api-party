3513adbd94b792ff723e0982ab6e34b96e168375
Your Full Name <youremailaddress@example.com>
2017-05-30 10:57:55 -0400

    add github nav link and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -12,8 +12,14 @@ class App extends Component {
             <h3>Aint no party like an</h3>
             <h1>API Party</h1>
           </div>
+          <ul className='nav-links'>
+            <li>
+              <NavLink to='/github'>GitHub API></NavLink>
+            </li>
+          </ul>
         </div>
         <Switch>
+          <Route path="/github" render={() => <h1>GitHub</h1>}/>
           <Route render={() => <p>To get started, click one of the links above </p>}/>
         </Switch>
       </div>
