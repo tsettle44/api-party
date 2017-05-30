288ee93e20aa742ac9919afbc194eb55b89a02b7
Your Full Name <youremailaddress@example.com>
2017-05-30 13:35:44 -0400

    Add instagram user route

--- src/Instagram.js
+++ src/Instagram.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import InstagramUser from './InstagramUser'
 import './Instagram.css'
 
 class Instagram extends Component {
+    state = {
+            username: ''
+    }
+
+    handleChange = (ev) => {
+        const username = ev.currentTarget.value
+        this.setState({ username })
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/instagram/${this.state.username}`)
+    }
+
     render() {
         return(
             <div className="instagram">
                 <img className="instagram-logo"src="https://static.wixstatic.com/media/c5ea1b_90fc60e229944c51a89836eaefcc0373.png" alt="instgram"/>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input 
                         type="text" 
-                        value=''/>
+                        value={this.state.username}
+                        onChange={this.handleChange}/>
                     </div>
                     <div>
                         <button type='submit'>Look up instagram user</button>
@@ -19,7 +35,7 @@ class Instagram extends Component {
                 </form>
 
                 <Route exact path='/instgram'render={({ match }) => <h3>Please enter a username to search on Github</h3>}/>
-                {/*<Route path='/instgram/:username' component={InstagramUser} /> */}
+                <Route path='/instagram/:username' component={InstagramUser} /> 
             </div>
         )
     }
