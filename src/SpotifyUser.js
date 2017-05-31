fd17638cbcaa3c099e7ccd5c239e72e821a74c01
Your Full Name <youremailaddress@example.com>
2017-05-30 22:35:57 -0400

    Spotify display button and token auth

---
+++ src/SpotifyUser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SpotifyUser extends Component {
+    render() {
+        return(
+            <h1>User</h1>
+        )
+    }
+}
+
+export default SpotifyUser 
